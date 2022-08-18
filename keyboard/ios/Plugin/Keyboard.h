d7b21fac67910294a1ca52caee28264cc9d9f33b
NKDuy <kn145660@gmail.com>
2022-08-18 20:38:57 +0700

    feat: add keyboard plugin

---
+++ keyboard/ios/Plugin/Keyboard.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+#import <Jigra/JIGPlugin.h>
+#import <Jigra/JIGBridgedPlugin.h>
+
+
+@class JIGPluginCall;
+
+@interface KeyboardPlugin : JIGPlugin <JIGBridgedPlugin>
+
+@end
+
---
+++ keyboard/ios/Plugin/KeyboardPlugin.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <Jigra/Jigra.h>
+
+// Define the plugin using the JIG_PLUGIN Macro, and
+// each method the plugin supports using the JIG_PLUGIN_METHOD macro.
+JIG_PLUGIN(KeyboardPlugin, "Keyboard",
+           JIG_PLUGIN_METHOD(show, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(hide, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(setAccessoryBarVisible, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(setStyle, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(setResizeMode, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(setScroll, JIGPluginReturnPromise);
+           JIG_PLUGIN_METHOD(removeAllListeners, JIGPluginReturnNone);
+)
