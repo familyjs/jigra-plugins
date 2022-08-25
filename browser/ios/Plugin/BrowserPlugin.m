7e02e8373113d4a7b2b1ae32305e63767c0dfe83
NKDuy <kn145660@gmail.com>
2022-08-25 16:52:05 +0700

    feat: add browser plugin

---
+++ browser/ios/Plugin/BrowserPlugin.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <Jigra/Jigra.h>
+
+JIG_PLUGIN(JIGBrowserPlugin, "Browser",
+  JIG_PLUGIN_METHOD(open, JIGPluginReturnPromise);
+  JIG_PLUGIN_METHOD(close, JIGPluginReturnPromise);
+  JIG_PLUGIN_METHOD(removeAllListeners, JIGPluginReturnNone);
+)
---
+++ browser/src/index.ts
@@ -0,0 +1,10 @@
+import { registerPlugin } from '@jigra/core';
+
+import type { BrowserPlugin } from './definitions';
+
+const Browser = registerPlugin<BrowserPlugin>('Browser', {
+  web: () => import('./web').then(m => new m.BrowserWeb()),
+});
+
+export * from './definitions';
+export { Browser };
