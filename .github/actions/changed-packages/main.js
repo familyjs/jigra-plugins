fa584d919b17954bab952f355d5407226f13416e
NKDuy <nkduy.dev@gmail.com>
2024-06-28 18:33:03 +0700

    chore: update changed-packages action

--- .github/actions/changed-packages/main.js
+++ .github/actions/changed-packages/main.js
@@ -1,18 +1,37 @@
-require = require('esm')(module);
-
 const { relative, resolve } = require('path');
 const core = require('@actions/core')
 
-const { root } = require('../../../scripts/lib/repo.mjs');
-const { execute } = require('../../../scripts/lib/cli.mjs');
-const { ls } = require('../../../scripts/lib/lerna.mjs');
+async function main() {
+  const {root} = await import('../../../scripts/lib/repo.mjs');
+  const {execute} = await import('../../../scripts/lib/cli.mjs');
+  const {ls} = await import('../../../scripts/lib/lerna.mjs');
 
-execute(async () => {
-  const files = JSON.parse(core.getInput('files', '[]')).map(f => resolve(root, f));
-  const packages = await ls();
-  const changedPackages = packages.filter(pkg => files.some(f => f.startsWith(pkg.location)));
-  const paths = JSON.stringify(changedPackages.map(pkg => relative(root, pkg.location)));
+  execute(async () => {
+    let filesInput = core.getInput('files', '[]');
+    let files;
 
-  core.info(`Changed package paths: ${paths}`);
-  core.setOutput('paths', paths);
+    try {
+      files = filesInput ? JSON.parse(filesInput) : [];
+    } catch (error) {
+      core.setFailed(`Invalid JSON input for 'files': ${error.message}`);
+      return;
+    }
+
+    if (!Array.isArray(files)) {
+      core.setFailed("The 'files' input is not an array.");
+      return;
+    }
+
+    const resolvedFiles = files.map(f => resolve(root, f));
+    const packages = await ls();
+    const changedPackages = packages.filter(pkg => resolvedFiles.some(f => f.startsWith(pkg.location)));
+    const paths = JSON.stringify(changedPackages.map(pkg => relative(root, pkg.location)));
+
+    core.info(`Changed package paths: ${paths}`);
+    core.setOutput('paths', paths);
+  });
+}
+
+main().catch(error => {
+  core.setFailed(error.message);
 });
