31bd74fdaac8e8dda0a6930557ccfbc52abc6c64
NKDuy <nkduy.dev@gmail.com>
2024-06-28 18:41:01 +0700

    chore: update changed-packages action

---
+++ .github/actions/changed-packages/main.mjs
@@ -0,0 +1,15 @@
+import { relative, resolve } from 'path';
+import * as core from '@actions/core';
+import { root } from '../../../scripts/lib/repo.mjs';
+import { execute } from '../../../scripts/lib/cli.mjs';
+import { ls } from '../../../scripts/lib/lerna.mjs';
+
+execute(async () => {
+  const files = JSON.parse(core.getInput('files', '[]')).map(f => resolve(root, f));
+  const packages = await ls();
+  const changedPackages = packages.filter(pkg => files.some(f => f.startsWith(pkg.location)));
+  const paths = JSON.stringify(changedPackages.map(pkg => relative(root, pkg.location)));
+
+  core.info(`Changed package paths: ${paths}`);
+  core.setOutput('paths', paths);
+});
